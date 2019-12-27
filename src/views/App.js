3e66e705ee460ed22851bc21f1401083348b8461
Alex Marey <armv26@mst.edu>
2019-12-27 16:40:33 -0500

    updated folder structure

---
+++ src/views/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TournamentForm from '../components';
+
+function App() {
+  return (
+    <div className="App">
+      <TournamentForm/>
+    </div>
+  );
+}
+
+export default App;
